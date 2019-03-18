d74e9911e04d2ed0f106968b571151cebcab653e
Hameed Khan <vjhameed3@gmail.com>
2019-03-18 08:58:18 -0700

    fixes

--- server/main.js
+++ server/main.js
@@ -4,30 +4,30 @@ import { Roles } from "meteor/alanning:roles";
 import "../imports/api/users/tasks";
 import "../imports/api/Categories/index";
 
-// Accounts.onCreateUser((options, user) => {
-//   Meteor.setTimeout(function() {
-//     Roles.addUsersToRoles(user._id, "simple-new-user");
-//   }, 0);
+Accounts.onCreateUser((options, user) => {
+  Meteor.setTimeout(function() {
+    Roles.addUsersToRoles(user._id, "simple-new-user");
+  }, 0);
 
-//   return user;
-// });
+  return user;
+});
 
 Meteor.startup(() => {
-  var users = [
-    {
-      name: "Hameed Khan",
-      email: "hameed@immasive.com",
-      roles: ["admin"]
-    }
-  ];
-  var id, user;
-  id = Accounts.createUser({
-    email: users[0].email,
-    password: "securesystem",
-    profile: { name: users[0].name }
-  });
-  // email verification
-  Meteor.users.update({ _id: id }, { $set: { "emails.0.verified": true } });
-  Roles.addUsersToRoles(id, users[0].roles);
-  console.log(Roles.userIsInRole(id, "admin"));
+  // var users = [
+  //   {
+  //     name: "Hameed Khan",
+  //     email: "hameed@immasive.com",
+  //     roles: ["admin"]
+  //   }
+  // ];
+  // var id, user;
+  // id = Accounts.createUser({
+  //   email: users[0].email,
+  //   password: "securesystem",
+  //   profile: { name: users[0].name }
+  // });
+  // // email verification
+  // Meteor.users.update({ _id: id }, { $set: { "emails.0.verified": true } });
+  // Roles.addUsersToRoles(id, users[0].roles);
+  // console.log(Roles.userIsInRole(id, "admin"));
 });
